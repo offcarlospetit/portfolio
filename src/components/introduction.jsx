e2d5dd927fbedc81a0fe5c021926fe97e9de062d
Carlos Petit <ca.alberto.p@gmail.com>
2022-02-21 19:22:18 -0300

    add i18n translate

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,46 +1,62 @@
-import React, { Component } from 'react'
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-export default class Introduction extends Component {
-  render() {
-    return (
-      <div>
-        <section id="colorlib-hero" className="js-fullheight" data-section="home">
-          <div className="flexslider js-fullheight">
-            <ul className="slides">
-              <li style={{ backgroundImage: 'url(images/portada4.jpg)' }}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner js-fullheight">
-                        <div className="desc" style={{ marginTop: "330px" }}>
-                          <h1>Hi! <br />I'm Carlos,</h1>
-                          <p><a className="btn btn-primary btn-learn" href="certificados/CarlosPetitCV.pdf" download="">Download CV <i className="icon-download4"></i></a></p>
-                        </div>
+const Introduction = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <section id="colorlib-hero" className="js-fullheight" data-section="home">
+        <div className="flexslider js-fullheight">
+          <ul className="slides">
+            <li style={{ backgroundImage: "url(images/portada4.jpg)" }}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner js-fullheight">
+                      <div className="desc" style={{ marginTop: "330px" }}>
+                        <h1>
+                          {t("hi")} <br />
+                          {t("imname", { name: "Carlos" })}
+                        </h1>
+                        <p>
+                          <a
+                            className="btn btn-primary btn-learn"
+                            href="certificados/CarlosPetitCV.pdf"
+                            download=""
+                          >
+                            {t("download")} <i className="icon-download4"></i>
+                          </a>
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
-              </li>
-              <li style={{ backgroundImage: 'url(images/about3.jpg)' }}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc" style={{ marginTop: "330px" }}>
-                          <h1>I am <br />a Developer</h1>
-                        </div>
+              </div>
+            </li>
+            <li style={{ backgroundImage: "url(images/about3.jpg)" }}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner">
+                      <div className="desc" style={{ marginTop: "330px" }}>
+                        <h1>
+                          {t("im")}
+                          <br />
+                          {t("developer")}
+                        </h1>
                       </div>
                     </div>
                   </div>
                 </div>
-              </li>
-            </ul>
-          </div>
-        </section>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-      </div>
-    )
-  }
-}
+export default Introduction;
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,38 +1,85 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { useTranslation } from "react-i18next";
 
-export default class Sidebar extends Component {
-  render() {
-    return (
+const Sidebar = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
       <div>
-        <div>
-          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
-          <aside id="colorlib-aside" className="border js-fullheight">
-            {/* <div className="text-center">
+        <nav
+          href="#navbar"
+          className="js-colorlib-nav-toggle colorlib-nav-toggle"
+          data-toggle="collapse"
+          data-target="#navbar"
+          aria-expanded="false"
+          aria-controls="navbar"
+        >
+          <i />
+        </nav>
+        <aside id="colorlib-aside" className="border js-fullheight">
+          {/* <div className="text-center">
               <div className="author-img" style={{ backgroundImage: 'url(images/about.jpg)' }} />
               <h1 id="colorlib-logo"><a href="index.html">Dhruv Barochiya</a></h1>
               <span className="email"><i className="icon-mail"></i> Dhruv.barochiya34788@gmail.com</span>
             </div> */}
-            <div className="text-center">
-              <div className="author-img" style={{ backgroundImage: 'url(images/about3.jpg)' }}></div>
-              <h1 id="colorlib-logo"><a href="index.html">Carlos Petit</a></h1>
-              <span className="position">Mobile Developer in Santiago De Chile</span>
+          <div className="text-center">
+            <div
+              className="author-img"
+              style={{ backgroundImage: "url(images/about3.jpg)" }}
+            ></div>
+            <h1 id="colorlib-logo">
+              <a href="index.html">Carlos Petit</a>
+            </h1>
+            <span className="position">{t("aboutme")}</span>
+          </div>
+          <nav id="colorlib-main-menu" role="navigation" className="navbar">
+            <div id="navbar" className="collapse">
+              <ul>
+                <li class="active">
+                  <a href="/" data-nav-section="home">
+                    {t("home")}
+                  </a>
+                </li>
+                <li>
+                  <a href="/" data-nav-section="about">
+                    {t("about")}
+                  </a>
+                </li>
+                <li>
+                  <a href="/" data-nav-section="services">
+                    {t("services")}
+                  </a>
+                </li>
+                <li>
+                  <a href="/" data-nav-section="skills">
+                    {t("skills")}
+                  </a>
+                </li>
+                <li>
+                  <a href="/" data-nav-section="education">
+                    {t("education")}
+                  </a>
+                </li>
+                <li>
+                  <a href="/" data-nav-section="experience">
+                    {t("experience")}
+                  </a>
+                </li>
+                <li>
+                  <a href="/" data-nav-section="work">
+                    {t("work")}
+                  </a>
+                </li>
+                <li>
+                  <a href="/" data-nav-section="contact">
+                    {t("contact")}
+                  </a>
+                </li>
+                <li></li>
+              </ul>
             </div>
-            <nav id="colorlib-main-menu" role="navigation" className="navbar">
-              <div id="navbar" className="collapse">
-                <ul>
-                  <li class="active"><a href="/" data-nav-section="home">Home</a></li>
-                  <li><a href="/" data-nav-section="about">About</a></li>
-                  <li><a href="/" data-nav-section="services">Services</a></li>
-                  <li><a href="/" data-nav-section="skills">Skills</a></li>
-                  <li><a href="/" data-nav-section="education">Education</a></li>
-                  <li><a href="/" data-nav-section="experience">Experience</a></li>
-                  <li><a href="/" data-nav-section="work">Work</a></li>
-                  <li><a href="/" data-nav-section="contact">Contact</a></li>
-                  <li></li>
-                </ul>
-              </div>
-            </nav>
-            {/* <nav id="colorlib-main-menu">
+          </nav>
+          {/* <nav id="colorlib-main-menu">
               <ul>
                 <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
                 <li><a href="https://twitter.com/ddbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
@@ -42,18 +89,31 @@ export default class Sidebar extends Component {
                 <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
               </ul>
             </nav> */}
-            <div className="colorlib-footer">
-              <p><small>
-                Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
-                Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration
-              </small></p>
-              <p><small>
-              Copyright {new Date().getFullYear()} All rights reserved
-              </small></p>
-            </div>
-          </aside>
-        </div>
+          <div className="colorlib-footer">
+            <p>
+              <small>
+                {t("made")} <i className="icon-heart" aria-hidden="true" />{" "}
+                {t("and")} <i className="icon-beer" aria-hidden="true"></i>
+                <br></br>
+                {t("thanks")}{" "}
+                <a
+                  href="https://colorlib.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Colorlib
+                </a>{" "}
+                {t("forInspiration")}
+              </small>
+            </p>
+            <p>
+              
+            </p>
+          </div>
+        </aside>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
+
+export default Sidebar;
